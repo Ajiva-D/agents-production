872f9aff63343bd33c34dc3a99320a1562c6ba7c
David Aji <davidenochaji@gmail.com>
2025-01-27 09:36:39 +0100

    add evals

---
+++ evals/experiments/allTools.eval.ts
@@ -0,0 +1,45 @@
+import { runLLM } from '../../src/llm'
+import { dadJokeToolDefinition } from '../../src/tools/dadJoke'
+import { generateImageToolDefinition } from '../../src/tools/generateImage'
+import { redditToolDefinition } from '../../src/tools/reddit'
+import { runEval } from '../evalTools'
+import { ToolCallMatch } from '../scorers'
+
+const createToolCallMessage = (toolName: string) => ({
+  role: 'assistant',
+  tool_calls: [
+    {
+      type: 'function',
+      function: { name: toolName },
+    },
+  ],
+})
+
+const allTools = [
+  redditToolDefinition,
+  dadJokeToolDefinition,
+  generateImageToolDefinition,
+]
+
+runEval('allTools', {
+  task: (input) =>
+    runLLM({
+      messages: [{ role: 'user', content: input }],
+      tools: allTools,
+    }),
+  data: [
+    {
+      input: 'Tell me a funny dad joke',
+      expected: createToolCallMessage(dadJokeToolDefinition.name),
+    },
+    {
+      input: 'take a photo of mars',
+      expected: createToolCallMessage(dadJokeToolDefinition.name),
+    },
+    {
+      input: "what's the most upvoted post on reddit",
+      expected: createToolCallMessage(redditToolDefinition.name),
+    },
+  ],
+  scorers: [ToolCallMatch],
+})
